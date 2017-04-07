9050dcaf95444fc22a4c0396e8404ebcac2973e1
Allen-Jian <allenjian94@gmail.com>
2017-04-07 14:58:37 +1200

    Add files via upload

--- index.android.js
+++ index.android.js
@@ -4,50 +4,129 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Platform,
+    TouchableOpacity,
 } from 'react-native';
 
-export default class Basement_Theatre extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+//get screen info
+var Dimensions = require('Dimensions');
+var width = Dimensions.get('window').width;
+
+class BasementTheatre extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+
+                {/*use View to make username txt box*/}
+                <View style={styles.textInputViewStyle}>
+                    <TextInput
+                        style={styles.textInputStyle}
+                        //placeholder
+                        placeholder='username'
+                        textAlign='center'
+                        underlineColorAndroid={'transparent'} />
+                </View>
+                {/*password input box*/}
+                <View style={styles.textInputViewStyle}>
+                    <TextInput
+                        style={styles.textInputStyle}
+                        placeholder='password'
+                        textAlign='center'
+                        //invisibility of password
+                        secureTextEntry={true}/>
+                </View>
+
+                {/*clickable button*/}
+                <TouchableOpacity onPress={()=>{alert('Login')}}>
+                    {/*login button*/}
+                    <View style={styles.textLoginViewStyle}>
+                        <Text style={styles.textLoginStyle}>Login</Text>
+                    </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>{alert('Register')}}>
+                    {/*register button*/}
+                    <View style={styles.textLoginViewStyle}>
+                        <Text style={styles.textLoginStyle}>Register</Text>
+                    </View>
+                </TouchableOpacity>
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        //set to full screen
+        flex: 1,
+        backgroundColor: 'skyblue',
+        paddingTop: 140,
+
+
+    },
+
+    textInputViewStyle: {
+
+        width: width - 30,
+        height: 45,
+
+        borderRadius: 18,
+
+        borderColor: 'blue',
+
+        borderWidth: 3,
+
+        paddingLeft: 10,
+        paddingRight: 10,
+
+        marginTop: 10,
+        marginLeft: 20,
+        marginRight: 20,
+
+        alignSelf: 'center',
+
+
+    },
+    //txt box style
+    textInputStyle: {
+        width: width - 30,
+        height: 35,
+        backgroundColor: '#00000000',
+        alignSelf: 'center',
+
+        marginTop: Platform.OS === 'ios' ? 4 : 8,
+    },
+
+    //login button style
+    textLoginViewStyle: {
+        width:  200,
+        height: 45,
+        backgroundColor: 'red',
+        borderRadius: 20,
+        marginTop: 30,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    //"Login" style
+    textLoginStyle: {
+        fontSize: 18,
+        color: 'white',
+
+
+    },
+
 });
 
-AppRegistry.registerComponent('Basement_Theatre', () => Basement_Theatre);
+//module.exports = BasementTheatre;
+
+
+AppRegistry.registerComponent('BasementTheatre', () => BasementTheatre);
