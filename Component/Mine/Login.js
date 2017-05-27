a41e8399c18046981bd2017afb312b87a8886c46
Jasper-Jian <jasperjiangg@gmail.com>
2017-05-27 16:23:49 +1200

    Google Login Created

--- Component/Mine/Login.js
+++ Component/Mine/Login.js
@@ -19,83 +19,147 @@ var CheckLogin = 0;
 //get Screen width
 var Dimensions = require('Dimensions');
 var width = Dimensions.get('window').width;
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
+  componentDidMount() {
+    this._setupGoogleSignin();
+  }
+  render() {
+    if (!this.state.user) {
+      return (
+          <View style={styles.container}>
+            <Image source={{uri: 'https://basement-theatre.squarespace.com/assets/images/logo-expanded.png'}} style={styles.itemStyle}>
+              {/*use View to make username txt box*/}
+              <View style={styles.LoginBox}>
+              <View style={styles.textInputViewStyle}>
+                  <TextInput
+                      style={styles.textInputStyle}
+                      //placeholder
+                      placeholder='username'
+                      textAlign='center'
+                      underlineColorAndroid={'transparent'} />
+              </View>
 
-var Login = React.createClass({
-    render() {
-        return (
-            <View style={styles.container}>
-              <Image source={{uri: 'https://basement-theatre.squarespace.com/assets/images/logo-expanded.png'}} style={styles.itemStyle}>
-                {/*use View to make username txt box*/}
-                <View style={styles.LoginBox}>
-                <View style={styles.textInputViewStyle}>
-                    <TextInput
-                        style={styles.textInputStyle}
-                        //placeholder
-                        placeholder='username'
-                        textAlign='center'
-                        underlineColorAndroid={'transparent'} />
-                </View>
+              {/*password input box*/}
+              <View style={styles.textInputViewStyle}>
+                  <TextInput
+                      style={styles.textInputStyle}
+                      placeholder='password'
+                      textAlign='center'
+                      //invisibility of password
+                      secureTextEntry={true} />
+              </View>
 
-                {/*password input box*/}
-                <View style={styles.textInputViewStyle}>
-                    <TextInput
-                        style={styles.textInputStyle}
-                        placeholder='password'
-                        textAlign='center'
-                        //invisibility of password
-                        secureTextEntry={true} />
-                </View>
+              {/*clickable button*/}
+              <TouchableOpacity onPress={() => { alert('Login') }}>
+                  {/*login button*/}
+                  <View style={styles.textLoginViewStyle}>
+                      <Text style={styles.textLoginStyle}>Login</Text>
+                  </View>
+              </TouchableOpacity>
 
-                {/*clickable button*/}
-                <TouchableOpacity onPress={() => { alert('Login') }}>
-                    {/*login button*/}
-                    <View style={styles.textLoginViewStyle}>
-                        <Text style={styles.textLoginStyle}>Login</Text>
-                    </View>
-                </TouchableOpacity>
+              {/*register button*/}
+              <TouchableOpacity onPress={() => { this._ToRegister()}}>
+                 <View style={styles.textLoginViewStyle}>
+                      <Text style={styles.textLoginStyle}>Register</Text>
+                  </View>
+              </TouchableOpacity>
 
-                {/*register button*/}
-                <TouchableOpacity onPress={() => { this._ToRegister()}}>
-                   <View style={styles.textLoginViewStyle}>
-                        <Text style={styles.textLoginStyle}>Register</Text>
-                    </View>
-                </TouchableOpacity>
+              {/*Facebook Login button*/}
+              <TouchableOpacity onPress={() => { this._fbAuth()}}>
+                  {/*register button*/}
+                  <View style={styles.textLoginViewStyle}>
+                      <Text style={styles.textLoginStyle}>Login with Facebook</Text>
+                  </View>
+              </TouchableOpacity>
+               <GoogleSigninButton style={styles.textLoginViewStyle} color={GoogleSigninButton.Color.Light} size= {GoogleSigninButton.Size.Icon} onPress={() => { this._signIn(); }}/>
+               </View>
 
-                {/*Facebook Login button*/}
-                <TouchableOpacity onPress={() => { this._fbAuth()}}>
-                    {/*register button*/}
-                    <View style={styles.textLoginViewStyle}>
-                        <Text style={styles.textLoginStyle}>Login with Facebook</Text>
-                    </View>
-                </TouchableOpacity>
-                 <GoogleSigninButton style={styles.textLoginViewStyle} color={GoogleSigninButton.Color.Light} size= {GoogleSigninButton.Size.Icon} onPress={() => {GoogleSignin.signIn()}}/>
-                 </View>
-                 </Image>
-            </View>
-        );
-    },
-    _fbAuth() {
-        LoginManager.logInWithReadPermissions(['public_profile']).then(function (result) {
-            if (result.isCancelled) {
-                console.log('Login cancelled');
-            } else {
-                console.log('Login success:' + result.grantedPermissions);
-            }
-        },
-            function (error) {
-                console.log('An error occured:' + error);
-            });
-    },
-    //Goto Register
-    _ToRegister() {
-        this.props.navigator.push(
-            {
-                component: Register,//Navigate page
-                title: 'Register with email'
-            }
-        );
+               </Image>
+
+          </View>
+
+      );
+  }
+  if (this.state.user) {
+     return (
+       <View style={styles.container}>
+         <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>Welcome {this.state.user.name}</Text>
+         <Text>Your email is: {this.state.user.email}</Text>
+
+         <TouchableOpacity onPress={() => {this._signOut(); }}>
+           <View style={{marginTop: 50}}>
+             <Text>Log out</Text>
+           </View>
+         </TouchableOpacity>
+       </View>
+     );
+   }
+}
+  async _setupGoogleSignin() {
+    try {
+      await GoogleSignin.hasPlayServices({ autoResolve: true });
+      await GoogleSignin.configure({
+        webClientId: '624986151461-ghge0s70j54dism65b9v7k2gfpjq411i.apps.googleusercontent.com',
+        offlineAccess: false
+      });
+
+      const user = await GoogleSignin.currentUserAsync();
+      console.log(user);
+      this.setState({user});
     }
-});
+    catch(err) {
+      console.log("Play services error", err.code, err.message);
+    }
+  }
+
+  _signOut() {
+    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+      this.setState({user: null});
+    })
+    .done();
+  }
+
+  _signIn() {
+    GoogleSignin.signIn().then((user) => {
+      alert(user);
+      this.setState({user: user});
+    })
+    .catch((err) => {
+      alert('WRONG SIGNIN', err);
+    })
+    .done();
+  }
+
+
+  _fbAuth() {
+      LoginManager.logInWithReadPermissions(['public_profile']).then(function (result) {
+          if (result.isCancelled) {
+              alert('Login cancelled');
+          } else {
+              alert('Login success:' + result.grantedPermissions);
+          }
+      },
+          function (error) {
+              console.log('An error occured:' + error);
+          });
+  }
+  //Goto Register
+  _ToRegister() {
+      this.props.navigator.push(
+          {
+              component: Register,//Navigate page
+              title: 'Register with email'
+          }
+      );
+  }
+}
+
 
 const styles = StyleSheet.create({
     container: {
@@ -148,4 +212,3 @@ const styles = StyleSheet.create({
       resizeMode: 'stretch'
     }
 });
-module.exports = Login;
