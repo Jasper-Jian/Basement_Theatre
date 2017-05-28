c227a175a1501e83ec1cc651ce5ab6ea9d64aed7
Jasper-Jian <jasperjiangg@gmail.com>
2017-05-28 19:09:02 +1200

    Edited Login Page
    Changed abit and Basic After Login Page finish develop

--- Component/Mine/BTMine.js
+++ Component/Mine/BTMine.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Platform,
-    TouchableOpacity
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Platform,
+  TouchableOpacity,
+  Navigator
 } from 'react-native';
+import {LoginManager} from 'react-native-fbsdk';
 import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 
 //get screen info
@@ -15,70 +19,208 @@ var width = Dimensions.get('window').width;
 
 /*import external component*/
 import Register from './Register';
-import Login from './Login';
+import Admin from './Admin';
 export default class Mine extends Component{
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.textAlert}>
-                    Seem like you haven‘t Login.
-                </Text>
-                <TouchableOpacity onPress={() => { this.pushToLogin() }}>
-                    {/*login button*/}
-                    <View style={styles.textLoginViewStyle}>
-                        <Text style={styles.textLoginStyle}>GotoLoginPage</Text>
-                    </View>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => { this.pushToRegister() }}>
-                    {/*register button*/}
-                    <View style={styles.textLoginViewStyle}>
-                        <Text style={styles.textLoginStyle}>Register</Text>
-                    </View>
-                </TouchableOpacity>
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+  }
+  componentDidMount() {
+    this._setupGoogleSignin();
+  }
+  render() {
+    if (!this.state.user) {
+      return (
+          <View style={styles.container}>
+            <Image source={{uri: 'https://basement-theatre.squarespace.com/assets/images/logo-expanded.png'}} style={styles.itemStyle}>
+              {/*use View to make username txt box*/}
+              <View style={styles.LoginBox}>
+              <View style={styles.textInputViewStyle}>
+                  <TextInput
+                    ref="inputLoginName"
+                    autoFocus={true}
+                      style={styles.textInputStyle}
+                      //placeholder
+                      placeholder='Please Enter Your Username or Email'
+                      textAlign='center'
+                      underlineColorAndroid={'transparent'}
+                      onChangeText={(input) => this.setState({username: input})}/>
+              </View>
 
-            </View>
-        );
+              {/*password input box*/}
+              <View style={styles.textInputViewStyle}>
+                  <TextInput
+                    ref="inputLoginPwd"
+                    clearTextOnFocus={true}
+                    clearButtonMode="while-editing"
+                      style={styles.textInputStyle}
+                      placeholder='Please Enter Your Password'
+                      textAlign='center'
+                      //invisibility of password
+                      secureTextEntry={true}
+                    onChangeText={(input) => this.setState({userpwd: input})}/>
+              </View>
+
+              {/*clickable button*/}
+              <TouchableOpacity onPress={()=>{this.loginInMainpage()}}>
+                  {/*login button*/}
+                  <View style={styles.textLoginViewStyle}>
+                      <Text style={styles.textLoginStyle}>Login</Text>
+                  </View>
+              </TouchableOpacity>
+
+              {/*register button*/}
+              <TouchableOpacity onPress={() => { this._ToRegister()}}>
+                 <View style={styles.textLoginViewStyle}>
+                      <Text style={styles.textLoginStyle}>Register</Text>
+                  </View>
+              </TouchableOpacity>
+
+              {/*Facebook Login button*/}
+              <TouchableOpacity onPress={() => { this._fbAuth()}}>
+                  {/*register button*/}
+                  <View style={styles.textLoginViewStyle}>
+                      <Text style={styles.textLoginStyle}>Login with Facebook</Text>
+                  </View>
+              </TouchableOpacity>
+               <GoogleSigninButton style={styles.textLoginViewStyle} color={GoogleSigninButton.Color.Light} size= {GoogleSigninButton.Size.Icon} onPress={() => { this._signIn(); }}/>
+               </View>
+
+               </Image>
+
+          </View>
+
+      );
+  }
+  if (this.state.user) {
+     return (
+       <View style={styles.container}>
+         <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>Welcome {this.state.user.name}</Text>
+         <Text>Your email is: {this.state.user.email}</Text>
+
+         <TouchableOpacity onPress={() => {this._signOut(); }}>
+           <View style={{marginTop: 50}}>
+             <Text>Log out</Text>
+           </View>
+         </TouchableOpacity>
+       </View>
+     );
+   }
+}
+    loginInMainpage() {
+    this.refs.inputLoginName.blur();
+    this.refs.inputLoginPwd.blur();
+    this.props.navigator.push({
+        component: Admin,
+        params: {
+            logName: this.state.username,
+            logPwd: this.state.userpwd,
+            parentComponent: this,...this.props
+        },
+    });
+    }
+    setLoginName(input) {
+        this.setState = {inputName: input}
     }
 
-    //Goto Register
-    pushToRegister() {
-        this.props.navigator.push(
-            {
-                component: Register,//Navigate page
-                title: 'Register with email'
-            }
-        );
+    setLoginPwd(input) {
+        this.setState = {inputPwd: input}
     }
-    pushToLogin() {
-        this.props.navigator.push(
-            {
-                component: Login,//Navigate page
-                title: 'Login with email'
-            }
-        );
+  async _setupGoogleSignin() {
+    try {
+      await GoogleSignin.hasPlayServices({ autoResolve: true });
+      await GoogleSignin.configure({
+        webClientId: '624986151461-ghge0s70j54dism65b9v7k2gfpjq411i.apps.googleusercontent.com',
+        offlineAccess: false
+      });
+
+    const user = await GoogleSignin.currentUserAsync();
+      console.log(user);
+      this.setState({user});
     }
-};
+    catch(err) {
+      console.log("Play services error", err.code, err.message);
+    }
+  }
+
+  _signOut() {
+    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+      this.setState({user: null});
+    })
+    .done();
+  }
+
+  _signIn() {
+    GoogleSignin.signIn().then((user) => {
+      this.setState({user: user});
+    })
+    .catch((err) => {
+      alert('WRONG SIGNIN', err);
+    })
+    .done();
+  }
+
+
+  _fbAuth() {
+      LoginManager.logInWithReadPermissions(['public_profile']).then(function (result) {
+          if (result.isCancelled) {
+              alert('Login cancelled');
+          } else {
+              alert('Login success:' + result.grantedPermissions);
+          }
+      },
+          function (error) {
+              console.log('An error occured:' + error);
+          });
+  }
+  //Goto Register
+  _ToRegister() {
+      this.props.navigator.push(
+          {
+              component: Register,//Navigate page
+              title: 'Register with email'
+          }
+      );
+  }
+}
+
 
 const styles = StyleSheet.create({
     container: {
         //set to full screen
         flex: 1,
-        backgroundColor: 'white',
-        paddingTop: 140,
+        backgroundColor:'rgba(250,227,98,1)',
+    },LoginBox:{
+      paddingTop:70,
+    },
+    textInputViewStyle: {
+        width: width - 90,
+        height: 45,
+        borderRadius: 10,
+        borderColor: 'rgba(114,83,52,1)',
+        borderWidth: 2,
+        marginTop:10,
+        alignSelf: 'flex-start',
+    },
+    //txt box style
+    textInputStyle: {
+        width: width - 30,
+        height: 35,
+        backgroundColor: '#00000000',
+        alignSelf: 'center',
+        marginTop: Platform.OS === 'ios' ? 4 : 8,
     },
 
-   textAlert: {
-       alignSelf:'center',
-       color:'black',
-       fontSize:20,
-   },
     //login button style
     textLoginViewStyle: {
         width: 200,
         height: 45,
-        backgroundColor: 'red',
+        backgroundColor: 'rgba(69,137,148,.9)',
         borderRadius: 20,
-        marginTop: 30,
+        marginTop: 20,
+        marginRight:60,
         alignSelf: 'center',
         justifyContent: 'center',
         alignItems: 'center',
@@ -87,5 +229,13 @@ const styles = StyleSheet.create({
     textLoginStyle: {
         fontSize: 18,
         color: 'white',
-    },
+    },itemStyle:{
+      position:'absolute',
+      left:10,
+      height:350,
+      margin:10,
+      marginRight:20,
+      width:width-30,
+      resizeMode: 'stretch'
+    }
 });
--- Component/Mine/Register.js
+++ Component/Mine/Register.js
@@ -7,104 +7,144 @@ import{
     TextInput,
     Platform,
     TouchableOpacity,
-    Navigator
+    Navigator,
+    Image
 } from 'react-native';
-var Mine = require('./BTMine');
-var Login = require('./Login');
+import Mine from './BTMine';
+import Admin from './Admin';
 var Dimensions = require('Dimensions');
 var width = Dimensions.get('window').width;
-var Register = React.createClass({
+export default class Register extends Component{
+  constructor(props) {
+      super(props);
+  }
     render(){
     return (
         <View style={styles.container}>
+            <Image source={{uri: 'https://basement-theatre.squarespace.com/assets/images/logo-expanded.png'}} style={styles.itemStyle}>
+                <View style={styles.LoginBox}>
             {/*use View to make username txt box*/}
             <View style={styles.textInputViewStyle}>
-                <TextInput
+                <TextInput ref="inputLoginName"
                     style={styles.textInputStyle}
                     //placeholder
-                    placeholder='Please enter your Email'
+                    placeholder='Please Enter Your Email'
                     textAlign='center'
+                    onChangeText={(input) => this.setState({username: input})}
                     underlineColorAndroid={'transparent'} />
             </View>
             {/*password input box*/}
             <View style={styles.textInputViewStyle}>
                 <TextInput
+                    ref="inputLoginPwd"
                     style={styles.textInputStyle}
-                    placeholder='Please enter your password'
+                    placeholder='Please Enter Your Password'
                     textAlign='center'
                     //invisibility of password
+                  onChangeText={(input) => this.setState({userpwd: input})}
                     secureTextEntry={true}/>
             </View>
             <View style={styles.textInputViewStyle}>
                 <TextInput
                     style={styles.textInputStyle}
-                    placeholder='Please enter your password again'
+                    placeholder='Please Confrim Your Password'
                     textAlign='center'
+
                     //invisibility of password
                     secureTextEntry={true}/>
             </View>
-            <TouchableOpacity onPress={()=>{alert('Register')}}>
+            <TouchableOpacity onPress={()=>this.loginInMainpage()}>
                 {/*register button*/}
                 <View style={styles.textLoginViewStyle}>
                     <Text style={styles.textLoginStyle}>Register</Text>
                 </View>
             </TouchableOpacity>
-        <TouchableOpacity onPress={this.pushBack()}>
-        <Text>Already have a Account? Go back to login Page</Text>
-        </TouchableOpacity>
-        </View>
+         <TouchableOpacity onPress={() => {this._pushBack(); }}>
+          <View>
+              <Text>Already have a Account? Go back to login Page</Text>
+          </View>
 
+        </TouchableOpacity>
+      </View>
+        </Image>
+        </View>
         );
-    },
-    pushBack() {
+    }
+    _pushBack() {
         this.props.navigator.pop(Mine);
     }
-});
+    loginInMainpage() {
+    this.refs.inputLoginName.blur();
+    this.refs.inputLoginPwd.blur();
+    this.props.navigator.resetTo({
+        component: Admin,
+        params: {
+            logNmae: this.state.username,
+            logPwd: this.state.userpwd,
+            parentComponent: this,
+            ...this.props
+        },
+    });
+    }
+
+    setLoginName(input) {
+        this.setState = {inputName: input}
+    }
+
+    setLoginPwd(input) {
+        this.setState = {inputPwd: input}
+    }
+}
 const styles = StyleSheet.create({
-    container: {
-        //set to full screen
-        flex: 1,
-        backgroundColor: 'skyblue',
-        paddingTop: 140,
-    },
+  container: {
+      //set to full screen
+      flex: 1,
+      backgroundColor:'rgba(174,221,129,1)',
+  },LoginBox:{
+    paddingTop:70,
+  },
+  textInputViewStyle: {
+      width: width - 90,
+      height: 45,
+      borderRadius: 10,
+      borderColor: 'rgba(114,83,52,1)',
+      borderWidth: 2,
+      marginTop:10,
+      alignSelf: 'flex-start',
+  },
+  //txt box style
+  textInputStyle: {
+      width: width - 30,
+      height: 35,
+      backgroundColor: '#00000000',
+      alignSelf: 'center',
+      marginTop: Platform.OS === 'ios' ? 4 : 8,
+  },
 
-    textInputViewStyle: {
-        width: width - 30,
-        height: 45,
-        borderRadius: 18,
-        borderColor: 'blue',
-        borderWidth: 3,
-        paddingLeft: 10,
-        paddingRight: 10,
-        marginTop: 10,
-        marginLeft: 20,
-        marginRight: 20,
-        alignSelf: 'center',
-    },
-    //txt box style
-    textInputStyle: {
-        width: width - 30,
-        height: 35,
-        backgroundColor: '#00000000',
-        alignSelf: 'center',
-        marginTop: Platform.OS === 'ios' ? 4 : 8,
-    },
-
-    //login button style
-    textLoginViewStyle: {
-        width:  200,
-        height: 45,
-        backgroundColor: 'red',
-        borderRadius: 20,
-        marginTop: 30,
-        alignSelf: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    //"Login" style
-    textLoginStyle: {
-        fontSize: 18,
-        color: 'white',
-    },
+  //login button style
+  textLoginViewStyle: {
+      width: 200,
+      height: 45,
+      backgroundColor: 'red',
+      borderRadius: 20,
+      marginTop: 20,
+      marginRight:60,
+      alignSelf: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  //"Login" style
+  textLoginStyle: {
+      fontSize: 18,
+      color: 'white',
+  },
+  itemStyle:{
+    position:'absolute',
+    left:10,
+    height:350,
+    margin:10,
+    marginRight:20,
+    width:width-30,
+    resizeMode: 'stretch'
+  }
 });
-module.exports = Register;
